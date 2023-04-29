0b267bf2a60fc93c5ff83de36ce3ed35bac9f0ca
Daniel Nolen <Daniel.Nolen@dowjones.com>
2023-04-29 15:57:24 -0400

    Update for 5 Inning Line to First 5 Innings bet types

--- dannyverse/get_bovada_odds.R
+++ dannyverse/get_bovada_odds.R
@@ -30,6 +30,17 @@ get_bovada_odds <- function(sport, league) {
     filter(period$live == FALSE) %>%
     mutate(bet_type = paste0(description, " - ", period$description)) %>%
     select(gamedate, AwayTeam, HomeTeam, AwayStartingPitcher, HomeStartingPitcher, bet_type, outcomes) %>%
+    filter(bet_type %in% c("Moneyline - Game", "Runline - Game", "Total - Game",
+                           "Will there be a run scored in the 1st inning - Game",
+                           "Moneyline - First 5 Innings", "Runline - First 5 Innings",
+                           "Total - First 5 Innings", "Spread - Game", "Spread - First 5 Innings", 
+                           "Total Runs O/U - Game", "Total Runs O/U - First 5 Innings",
+                           paste0("Total Runs O/U - ", AwayTeam, " - Game"), 
+                           paste0("Total Runs O/U - ", HomeTeam, " - Game"),
+                           paste0("Total Runs O/U - ", AwayTeam, " - First 5 Innings"), 
+                           paste0("Total Runs O/U - ", HomeTeam, " - First 5 Innings"),
+                           paste0(AwayTeam, " To Score - 1st Inning"), 
+                           paste0(HomeTeam, " To Score - 1st Inning"))) %>%
     unnest(outcomes) %>%
     mutate(Odds = as.numeric(price$american),
            SpreadTotal = price$handicap,
@@ -39,45 +50,34 @@ get_bovada_odds <- function(sport, league) {
                             TRUE ~ if_else(type == "A" | type == "O", "AOY", "HUN"))) %>%
     mutate(Odds = if_else(is.na(Odds), 100, Odds)) %>%
     select(gamedate, AwayTeam, HomeTeam, AwayStartingPitcher, HomeStartingPitcher, bet_type, type, Odds, any_of("SpreadTotal")) %>%
-    filter(bet_type %in% c("Moneyline - Game", "Runline - Game", "Total - Game",
-                           "Will there be a run scored in the 1st inning - Game",
-                           "Moneyline - 5 Inning Line", "Runline - 5 Inning Line",
-                           "Total - 5 Inning Line", "Spread - Game", "Spread - 5 Inning Line", 
-                           "Total Runs O/U - Game", "Total Runs O/U - 5 Inning Line",
-                           paste0("Total Runs O/U - ", AwayTeam, " - Game"), 
-                           paste0("Total Runs O/U - ", HomeTeam, " - Game"),
-                           paste0("Total Runs O/U - ", AwayTeam, " - 5 Inning Line"), 
-                           paste0("Total Runs O/U - ", HomeTeam, " - 5 Inning Line"),
-                           paste0(AwayTeam, " To Score - 1st Inning"), 
-                           paste0(HomeTeam, " To Score - 1st Inning"))) %>%
     mutate(bet_type = case_when(bet_type == "Spread - Game" ~ paste0("Alternate Runline - Game - ", abs(as.numeric(SpreadTotal))),
-                                bet_type == "Spread - 5 Inning Line" ~ paste0("Alternate Runline - 5 Inning Line - ", abs(as.numeric(SpreadTotal))),
+                                bet_type == "Spread - First 5 Innings" ~ paste0("Alternate Runline - First 5 Innings - ", abs(as.numeric(SpreadTotal))),
                                 bet_type == "Total Runs O/U - Game" ~ paste0("Alternate Total - Game - ", abs(as.numeric(SpreadTotal))),
-                                bet_type == "Total Runs O/U - 5 Inning Line" ~ paste0("Alternate Total - 5 Inning Line - ", abs(as.numeric(SpreadTotal))),
+                                bet_type == "Total Runs O/U - First 5 Innings" ~ paste0("Alternate Total - First 5 Innings - ", abs(as.numeric(SpreadTotal))),
                                 bet_type == paste0("Total Runs O/U - ", AwayTeam, " - Game") ~
                                   paste0("Team Total - ", AwayTeam, " - Game - ", abs(as.numeric(SpreadTotal))),
                                 bet_type == paste0("Total Runs O/U - ", HomeTeam, " - Game") ~
                                   paste0("Team Total - ", HomeTeam, " - Game - ", abs(as.numeric(SpreadTotal))),
-                                bet_type == paste0("Total Runs O/U - ", AwayTeam, " - 5 Inning Line") ~
-                                  paste0("Team Total - ", AwayTeam, " - 5 Inning Line - ", abs(as.numeric(SpreadTotal))),
-                                bet_type == paste0("Total Runs O/U - ", HomeTeam, " - 5 Inning Line") ~
-                                  paste0("Team Total - ", HomeTeam, " - 5 Inning Line - ", abs(as.numeric(SpreadTotal))),
+                                bet_type == paste0("Total Runs O/U - ", AwayTeam, " - First 5 Innings") ~
+                                  paste0("Team Total - ", AwayTeam, " - First 5 Innings - ", abs(as.numeric(SpreadTotal))),
+                                bet_type == paste0("Total Runs O/U - ", HomeTeam, " - First 5 Innings") ~
+                                  paste0("Team Total - ", HomeTeam, " - First 5 Innings - ", abs(as.numeric(SpreadTotal))),
                                 TRUE ~ bet_type)) %>%
     filter(bet_type %in% c("Moneyline - Game", "Runline - Game", "Total - Game",
                            "Will there be a run scored in the 1st inning - Game",
-                           "Moneyline - 5 Inning Line", "Runline - 5 Inning Line", 
-                           "Total - 5 Inning Line", "Alternate Runline - Game - 1.5",
+                           "Moneyline - First 5 Innings", "Runline - First 5 Innings", 
+                           "Total - First 5 Innings", "Alternate Runline - Game - 1.5",
                            "Alternate Runline - Game - 2.5", 
-                           "Alternate Runline - 5 Inning Line - 0.5",
-                           "Alternate Runline - 5 Inning Line - 1.5",
+                           "Alternate Runline - First 5 Innings - 0.5",
+                           "Alternate Runline - First 5 Innings - 1.5",
                            paste0("Alternate Total - Game - ", abs(as.numeric(SpreadTotal))),
-                           paste0("Alternate Total - 5 Inning Line - ", abs(as.numeric(SpreadTotal))),
+                           paste0("Alternate Total - First 5 Innings - ", abs(as.numeric(SpreadTotal))),
                            paste0(AwayTeam, " To Score - 1st Inning"), 
                            paste0(HomeTeam, " To Score - 1st Inning")) |
              str_detect(bet_type, paste0("Team Total - ", AwayTeam, " - Game")) |
              str_detect(bet_type, paste0("Team Total - ", HomeTeam, " - Game")) |
-             str_detect(bet_type, paste0("Team Total - ", AwayTeam, " - 5 Inning Line")) |
-             str_detect(bet_type, paste0("Team Total - ", HomeTeam, " - 5 Inning Line"))) %>%
+             str_detect(bet_type, paste0("Team Total - ", AwayTeam, " - First 5 Innings")) |
+             str_detect(bet_type, paste0("Team Total - ", HomeTeam, " - First 5 Innings"))) %>%
     distinct(gamedate, AwayTeam, HomeTeam, bet_type, type, .keep_all = TRUE) %>% 
     melt(id.vars = c("gamedate", "AwayTeam", "HomeTeam", "AwayStartingPitcher", "HomeStartingPitcher", "bet_type", "type")) %>%
     mutate(name = paste0(type, ".", variable)) %>%
